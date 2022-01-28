e22d0b77e806b375926c9214a0f26894448df758
Ardorax <valentin03dury@gmail.com>
2022-01-28 22:35:54 +0100

    data

--- webhook.py
+++ webhook.py
@@ -6,7 +6,12 @@ from json import JSONDecoder
 
 if __name__ == "__main__":
     summary = os.environ["INPUT_SUMMARY"]
+    print(summary)
     trace = os.environ["INPUT_TRACE"]
     print(trace)
+    url = os.environ["INPUT_URL"]
+    print(url)
+    username = os.environ["INPUT_USERNAME"]
+    print(username)
     json_summary = JSONDecoder().decode(summary)
     print(json_summary["major"])
