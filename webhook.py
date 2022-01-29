c902470fa855874a70f7fb8b91854c47b1645740
Ardorax <valentin03dury@gmail.com>
2022-01-29 17:22:32 +0100

    embed

--- webhook.py
+++ webhook.py
@@ -3,8 +3,9 @@ import os
 import requests
 from json import JSONDecoder
 
-def send_webhooks(Checker, url, color: int, file_path: str, name: str = "Normeur"):
-    payload = {"username": name, 'embeds': [
+def send_webhooks(Checker, url, color: int, file_path: str, desc: str,
+    name: str = "Normeur"):
+    payload = {"username": name, "description": desc , 'embeds': [
         {"title": "Votre résultat de moulinette :", "color": color, "fields": [
             {"name": "MAJOR", "value": Checker["major"], "inline": True},
             {"name": "MINOR", "value": Checker["minor"], "inline": True},
@@ -31,7 +32,8 @@ if __name__ == "__main__":
     trace = os.environ["INPUT_TRACE"]
     url = os.environ["INPUT_URL"]
     username = os.environ["INPUT_USERNAME"]
-    color = os.environ["INPUT_COLOR"]
+    color = os.environ["GITHUB_COLOR"]
+    desc = os.environ["GITHUB_DESCRIPTION"]
     print(os.environ["GITHUB_REPOSITORY_OWNER"])
     json_summary = JSONDecoder().decode(summary)
     send_webhooks(json_summary, url, int(color), trace, username)
